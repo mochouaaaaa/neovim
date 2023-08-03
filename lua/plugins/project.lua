b85b4897ee0442a604c1e42599e8808704000ee3
nanz <nanzhi@fengtaisec.com>
2023-08-04 03:25:55 +0800

    add project recognition

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,14 +1,27 @@
-return {
+local M = {
 	"ahmedkhalf/project.nvim",
-	config = function()
-		require("project_nvim").setup({
-			-- All the patterns used to detect root dir, when **"pattern"** is in
-			detection_methods = { "lsp", "pattern" },
-			-- detection_methods
-			patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
-			-- Show hidden files in telescope
-			show_hidden = true,
-			silent_chdir = false,
-		})
-	end,
+	cmd = { "BufWritePost", "BufReadPost", "InsertLeave" },
 }
+
+function M.config()
+	require("project_nvim").setup({
+		-- All the patterns used to detect root dir, when **"pattern"** is in
+		detection_methods = { "lsp", "pattern" },
+		-- detection_methods
+		patterns = {
+			".git",
+			".python-version",
+			"requirements.txt",
+			"go.mod",
+			".go-version",
+			".svn",
+			"Makefile",
+			"package.json",
+		},
+		-- Show hidden files in telescope
+		show_hidden = true,
+		silent_chdir = false,
+	})
+end
+
+return M
