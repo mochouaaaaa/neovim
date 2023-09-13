9d0572697442b89b309b8012e60a0a2407567586
nanz <nanzhi@fengtaisec.com>
2023-09-14 03:32:58 +0800

    add python server config

--- lua/plugins/lsp/lang/pyright/settings.lua
+++ lua/plugins/lsp/lang/pyright/settings.lua
@@ -3,8 +3,19 @@ return {
 		python = {
 			analysis = {
 				autoSearchPaths = true,
+				autoImportCompletions = true,
 				useLibraryCodeForTypes = true,
 				diagnosticMode = "workspace",
+			},
+			sortImports = {
+				path = "/Volumes/Code/tools/.pyenv/shims/isort",
+			},
+			formatting = {
+				blackPath = "/Volumes/Code/tools/.pyenv/shims/black",
+			},
+			inlayHints = {
+				functionReturnTypes = true,
+				variableTypes = true,
 			},
 		},
 	},
