14a2da4b7b71fd29e01d7606972c827188d229e6
mc <nn.mochou+github@gmail.com>
2024-01-06 00:59:01 +0800

    refactor: updata config terminal

--- lua/plugins/term/init.lua
+++ lua/plugins/term/init.lua
@@ -2,57 +2,79 @@ local lazygit, ranger
 
 local config = function(_, opts)
 	require("toggleterm").setup(opts)
-	local t = require("toggleterm.terminal").Terminal
-	lazygit = t:new({
-		cmd = "lazygit",
-		hidden = true,
-	})
-
-	ranger = t:new({
-		cmd = "lf",
-		hidden = true,
-	})
 end
 
 local flatten = {
 	"willothy/flatten.nvim",
-	lazy = false,
-	opts = {
-		callbacks = {
-			pre_open = function() -- Close toggleterm when an external open request is received
-				require("toggleterm").toggle(0)
-			end,
-			post_open = function(bufnr, winnr, ft)
-				if ft == "gitcommit" then
-					-- If the file is a git commit, create one-shot autocmd to delete it on write
-					-- If you just want the toggleable terminal integration, ignore this bit and only use the
-					-- code in the else block
-					vim.api.nvim_create_autocmd("BufWritePost", {
-						buffer = bufnr,
-						once = true,
-						callback = function()
-							-- This is a bit of a hack, but if you run bufdelete immediately
-							-- the shell can occasionally freeze
-							vim.defer_fn(function()
-								vim.api.nvim_buf_delete(bufnr, {})
-							end, 50)
-						end,
-					})
-					return
-				end
-				-- If it's a normal file, then reopen the terminal, then switch back to the newly opened window
-				-- This gives the appearance of the window opening independently of the terminal
-				-- vim.cmd.q()
-				require("toggleterm").toggle(0)
-				vim.api.nvim_set_current_buf(bufnr)
-				-- vim.api.nvim_set_current_win(winnr)
-			end,
-			block_end = function()
-				-- After blocking ends (for a git commit, etc), reopen the terminal
-				require("toggleterm").toggle(0)
-			end,
-		},
-	},
+	opts = function()
+		---@type Terminal?
+		local saved_terminal
+
+		return {
+			callbacks = {
+				should_block = function(argv)
+					return vim.tbl_contains(argv, "-b")
+				end,
+				pre_open = function() -- Close toggleterm when an external open request is received
+					-- require("toggleterm").toggle(0)
+					local term = require("toggleterm.terminal")
+					local termid = term.get_focused_id()
+					saved_terminal = term.get(termid)
+				end,
+				post_open = function(bufnr, winnr, ft, is_blocking)
+					if is_blocking and saved_terminal then
+						saved_terminal:close()
+					end
+
+					if ft == "gitcommit" then
+						-- If the file is a git commit, create one-shot autocmd to delete it on write
+						-- If you just want the toggleable terminal integration, ignore this bit and only use the
+						-- code in the else block
+						vim.api.nvim_create_autocmd("BufWritePost", {
+							buffer = bufnr,
+							once = true,
+							callback = function()
+								-- This is a bit of a hack, but if you run bufdelete immediately
+								-- the shell can occasionally freeze
+								vim.defer_fn(function()
+									vim.api.nvim_buf_delete(bufnr, {})
+								end, 50)
+							end,
+						})
+						return
+					end
+					-- If it's a normal file, then reopen the terminal, then switch back to the newly opened window
+					-- This gives the appearance of the window opening independently of the terminal
+					-- vim.cmd.q()
+					-- require("toggleterm").toggle(0)
+					-- vim.api.nvim_set_current_buf(bufnr)
+					-- vim.api.nvim_set_current_win(winnr)
+				end,
+				block_end = function()
+					-- After blocking ends (for a git commit, etc), reopen the terminal
+					-- require("toggleterm").toggle(0)
+					vim.schedule(function()
+						if saved_terminal then
+							saved_terminal:open()
+							saved_terminal = nil
+						end
+					end)
+				end,
+				pipe_path = function()
+					if vim.env.NVIM then
+						return vim.env.NVIM
+					end
+					if vim.env.KITTY_PID then
+						local addr = ("%s/%s"):format(vim.fn.stdpath("run"), "kitty.nvim-" .. vim.env.KITTY_PID)
+						if not vim.loop.fs_stat(addr) then
+							vim.fn.serverstart(addr)
+						end
+						return addr
+					end
+				end,
+			},
+		}
+	end,
 }
 
 local toggleterm = {
@@ -60,24 +82,7 @@ local toggleterm = {
 	cmd = { "ToggleTerm", "TermExec" },
 	keys = {
 		"<C-d>",
-		-- {
-		-- 	"<C-g>",
-		-- 	function()
-		-- 		lazygit:toggle()
-		-- 	end,
-		-- 	desc = "Toggle Lazygit",
-		-- },
-		-- {
-		-- 	"<C-w>e",
-		-- 	function()
-		-- 		ranger:toggle()
-		-- 	end,
-		-- 	desc = "Toggle Lazygit",
-		-- },
-		{ "<C-e>u", "<Cmd>ToggleTerm direction=horizontal<CR>", desc = "Toggle Terminal horizontal" },
-		{ "<C-e>o", "<Cmd>ToggleTerm direction=vertical<CR>", desc = "Toggle Terminal vertical" },
-		{ "<C-e>i", "<Cmd>ToggleTerm direction=float<CR>", desc = "Toggle Terminal float" },
-		-- { '<C-e><C-e>', '<Cmd>ToggleTermSendCurrentLine<CR>',       desc = 'Send Current Line' },
+		desc = "float terminal",
 	},
 	opts = {
 		size = function(term)
